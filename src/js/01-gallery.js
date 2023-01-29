4963f6c60f4b500dcc54f8b7e8515d96de902ac4
GannaKachalaba <111142980+GannaKachalaba@users.noreply.github.com>
2023-01-29 18:04:40 +0200

    hw
    hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Add imports above this line
+
 
 import { galleryItems } from './gallery-items';
 
@@ -29,4 +29,3 @@ new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionsDelay: 250,
 });
-
