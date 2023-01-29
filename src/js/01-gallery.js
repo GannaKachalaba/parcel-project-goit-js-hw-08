f4f03af62f7e66612164aed3b05738e676c4171a
GannaKachalaba <111142980+GannaKachalaba@users.noreply.github.com>
2023-01-29 17:47:43 +0200

    hw
    hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+const gallery = document.querySelector('.gallery');
+const gallarySetMarkup = createGalleryBox(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', gallarySetMarkup);
+
+function createGalleryBox(image) {
+  return image
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        /></a>
+        </div>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+let STORAGE_KEY = "feedback-form-state";
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input'),
+  message: document.querySelector('textarea'),
+};
+
+
+const formData = {};
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(evt) {
+  const key = evt.target.name;
+  formData[key] = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(formData);
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+};
+
+function noteForm() {
+  const data = localStorage.getItem(STORAGE_KEY);
+  const savedData = JSON.parse(data);
+
+  if (savedData.email) {
+    refs.email.value = savedData.email;
+  }
+
+   if (savedData.message) {
+     refs.message.value = savedData.message;
+   }
+};
+
+noteForm();
